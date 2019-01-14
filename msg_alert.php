2a47b65153a4066ea8759a5217a600779b8ad200
kornsub <kornsub.s@gmail.com>
2019-01-14 14:38:13 +0700

    Update msg_alert.php

--- msg_alert.php
+++ msg_alert.php
@@ -1,3 +1,43 @@
-<?php
+<?php 
 
+$access_token = "DBSY+rfxqfzGO26aJtKFoVPUqimxWuZ7DZaZX0kpltZVZ+sQrNMG+9qb1KjELbqXr/rvJb9LbH+xbUbP7KRMlW0ucrd0vFF4Nz7238rB59CUFKmFLT7Q1dplgAjBf1704K9jA9gLkdFVSscHcKKZqwdB04t89/1O/w1cDnyilFU=";
+
+			// Get text sent
+
+			$text = $_GET['text'];
+			//$To = "U50db648d0798f19ff5708e8a4e992b08";
+
+			//$text = "Robot Test Push Massage ?";
+			//$To = "C2f6acfc9f271f5422b1a4c1eb71b27b7";//GROUP
+      //ME
+      $To = "U50db648d0798f19ff5708e8a4e992b08";
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data = [
+				'to' => $To,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+
+
+echo "OK_push2";
 ?>
