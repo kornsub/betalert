1fa13abe5cb27b2eefce8fd36303a63909295a92
kornsub <kornsub.s@gmail.com>
2019-10-25 14:49:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,45 +14,52 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-
+			$text = "";
+			$msg = $event['message']['text'];
 			//$text = "User ID is : ".$event['source']['userId'] ."Group Id : ".$event['source']['groupId'];
 			
-			$text = "ว่าไง";
-			$msg = $event['message']['text'];
+			
+			
+			
 			$check_msg = explode(":",$msg);
-			if( count($check_msg) == 2 ){
-				if($check_msg[0] == 'ยกเลิก'){
-					$text = "ยกเลิก ".$check_msg[1];
+			if( count($check_msg) > 1 ){
+				if($check_msg[0] == 'ยกเลิกบิล'){
+					$text = "ยกเลิกบิล ".$check_msg[1]." เรียบร้อยแล้ว";
 				}
+			}elseif($msg == 'บอท'){
+				$text = "ว่าไง";
 			}
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+			
+			if( $text != '' ){
+				// Get replyToken
+				$replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				// Make a POST Request to Messaging API to reply to sender
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
 
-			echo $result . "\r\n";
+				echo $result . "\r\n";
+			}
 		}
 	}
 }
